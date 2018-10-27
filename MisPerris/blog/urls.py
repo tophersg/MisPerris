7933bf20bedeb0f9f2f6de2f92fdd59b7b3f2c79
marceloVO <43550661+marceloVO@users.noreply.github.com>
2018-10-27 19:08:53 -0300

    Djangov3

--- MisPerris/blog/urls.py
+++ MisPerris/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name= 'perris'
 
 urlpatterns = [
-    url(r'^$', views.post_list,name="home")
-]
+    url(r'^$', views.post_list,name="home"),
+    url(r'^usuario$', views.post_list,name="listar"),
 
+]
