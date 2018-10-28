bdbc8dd2520de661b495d45271b0fe660fae3e9c
John Doe <cri.sobarzo@alumnos.duoc.cl>
2018-10-28 19:17:39 -0300

    Registro de perro 1.2

--- MisPerris/adopcion/urls.py
+++ MisPerris/adopcion/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name= 'adopcion'
 
 urlpatterns = [
-   url(r'^adoptar$', views.adopcion_lista, name="lista"),
-
+   url(r'^adoptar/$', views.adopcion_lista, name="lista"),
+   url(r'^(?P<titulo>[\w-]+)/$', views.adopcion_detalles, name="detalles"),
 ]
--- MisPerris/adopcion/views.py
+++ MisPerris/adopcion/views.py
@@ -5,3 +5,8 @@ from .models import Perritos
 def adopcion_lista(request):
     adopciones = Perritos.objects.all().order_by('fecha_publicacion');
     return render(request, 'adopcion/adopcion_lista.html', { 'adopciones': adopciones })
+
+def adopcion_detalles(request, titulo):
+    # return HttpResponse(titulo)
+    adopcion = Perritos.objects.get(titulo=titulo)
+    return render(request, 'adopcion/adopcion_detalles.html', { 'adopcion': adopcion })
